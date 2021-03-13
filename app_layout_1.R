d1ac4d46bdb011cba312473151df7eb485216033
sophiabulcock <sophia.bulcock@gmail.com>
2021-03-13 11:07:47 -0800

    Did some manual formatting

--- app_layout_1.R
+++ app_layout_1.R
@@ -21,331 +21,183 @@ library(plotly)
   
 
 # title for entire dashboard
-title<-function(){
+title<-function() {
  
-return (htmlDiv(list(htmlH1("Board Game Trends Dashboard"))))
-  
-}
+return (htmlDiv(list(htmlH1("Board Game Trends Dashboard"))))}
 
 # description card tab 1
 description_card<- function(){
-  
 return (htmlDiv(
-  id="description-card",
-  list(
-    htmlH5("Welcome to our Board Games Dashboard"),
-    htmlDiv(
-      id="intro",
-      list("Explore board game trends over time based on category, mechanics \
+id="description-card",
+list(
+htmlH5("Welcome to our Board Games Dashboard"),
+htmlDiv(
+id="intro",
+list("Explore board game trends over time based on category, mechanics \
                     and publisher selection below. Also visualize the top categories,\
-                    mechanics and publishers by year using our interactive features."
-    ))
-  )
-))
-
-}
+                    mechanics and publishers by year using our interactive features.")))))}
 
 # control card for tab 1
-generate_control_card<- function(){
- 
+generate_control_card<- function() {
 return (htmlDiv(
-  id="control-card",
-  list(
-    htmlLabel("Select what you want to view:"),
-    htmlBr(),
-    htmlBr(),
-    # add radio button
-    htmlBr(),
-    htmlLabel("Select elements to view:"),
-    htmlBr(),
-    htmlBr()
+id="control-card",
+list(
+htmlLabel("Select what you want to view:"),
+htmlBr(),
+htmlBr(),
+# add radio button
+htmlBr(),
+htmlLabel("Select elements to view:"),
+htmlBr(),
+htmlBr())))}
    # add radio button dependent drop down,
   
-)))
-}
 
-generate_control_card_tab2 <- function(){
-  
+
+generate_control_card_tab2 <- function() {
 return(htmlDiv(
-  id="control-card2",
-  list(
-    htmlP(
-      "Please select any combination of categories, mechanics, publishers."
-                     
-    ),
-    htmlBr(),
-    htmlP("Please select categories:"),
-    # dropdown for categories goes here, select from dwranglingpart with dictionary for dropdowns at beggining of code
-    htmlBr(),
-    htmlP("Please select mechanics:"),
-    # dropdown for mechanics goes here, select from dwranglingpart with dictionary for dropdowns at beggining of code
-    htmlBr(),
-    htmlP("Please select publishers:"),
-    # dropdown for mechanics goes here, select from dwranglingpart with dictionary for dropdowns at beggining of code
-    htmlBr(),
-    htmlBr()
-  
-)))}
+id="control-card2",
+list(
+htmlP( "Please select any combination of categories, mechanics, publishers."),
+htmlBr(),
+htmlP("Please select categories:"),
+# dropdown for categories goes here, select from dwranglingpart with dictionary for dropdowns at beggining of code
+htmlBr(),
+htmlP("Please select mechanics:"),
+# dropdown for mechanics goes here, select from dwranglingpart with dictionary for dropdowns at beggining of code
+htmlBr(),htmlP("Please select publishers:"),
+# dropdown for mechanics goes here, select from dwranglingpart with dictionary for dropdowns at beggining of code
+htmlBr(),
+htmlBr())))}
 
 
 # lower description for tab 1
-lower_description<- function(){
-  
+lower_description<- function() {
 return (htmlDiv(
-  list(
-    htmlH4("Top 5 Categories, Mechanics and Publishers by Rating"),
-    htmlP(
-      "Two sets of bar charts with year range sliders are provided \
-                    to allow comparison for two different periods.",
-    ),
-    htmlBr(),
-    htmlP(
-      "Drag the year sliders below to select your year ranges and \
-                    compare the top 5 categories, mechanics and publishers \
-                    between time periods."
-    )
-  
-)))
-}
+list(htmlH4("Top 5 Categories, Mechanics and Publishers by Rating"),
+htmlP("Two sets of bar charts with year range sliders are provided \
+to allow comparison for two different periods.",),
+htmlBr(),
+htmlP("Drag the year sliders below to select your year ranges and \
+compare the top 5 categories, mechanics and publishers \
+between time periods."))))}
 
 # data set description for tab 1
-data_set_description <- function(){
- 
+data_set_description <- function() {
 return (htmlDiv(
- list(
-    htmlH4("Description of Dataset"),
-    htmlP(
-      " This dataset comes from the Board Game Geek website and \
-                    includes boardgames with descriptions, general game \
-                    details, publisher, and user ratings for 10,000 boardgames\
-                    published between 1950 and 2021."
-    )
-  
-)))}
+list(htmlH4("Description of Dataset"),
+htmlP(" This dataset comes from the Board Game Geek website and \
+includes boardgames with descriptions, general game \
+details, publisher, and user ratings for 10,000 boardgames\
+published between 1950 and 2021."))))}
 
 
 # card 1 containing the description and control card for tab 1
 first_card = dbcCard(
-  dbcCardBody(
-    
-      htmlDiv(
-        id="left-column",
-        className="four columns",
-        list(
-          description_card(),
-          htmlBr(),
-          generate_control_card())
-          
-        )
-      )
-    
-  )
+dbcCardBody(
+htmlDiv(id="left-column",
+className="four columns",list(description_card(),htmlBr(),generate_control_card()))))
 
 second_card = dbcCard(
-  dbcCardBody(
-  
-      htmlDiv(
-        list(
-          htmlH4("Board Game Ratings and Counts from 1950 to 2016"),
-          htmlP(
-            "Select either categories, mechanics or publishers.\
-                             Then select different elements to view on the\
-                                following two figures."
-          ))
+dbcCardBody(htmlDiv(list(htmlH4("Board Game Ratings and Counts from 1950 to 2016"),htmlP(
+"Select either categories, mechanics or publishers.\
+Then select different elements to view on the\
+following two figures.")))))
           # scatter plot tab 1 goes here
-         # histogram tab 1 goes here
-        
-  )
-))
+         # histogram tab 1 goes here)))
 
 # card 3 containing the lower description and collapsible data set description for tab 1
 third_card = dbcCard(
-  dbcCardBody(
-    
-      dbcCol(
-        id="bottom left row",
-        className="four columns",
-        list(lower_description()),
-      )
-    
-  )
-)
+dbcCardBody(
+dbcCol(id="bottom left row",className="four columns",list(lower_description()))))
 
 # card 4 containing the top slider and first faceted bar chart
 fourth_card = dbcCard(
-  dbcCardBody(list(
-    
-      
-        
-         
-            
-              htmlDiv(
-                list(
-              
-              htmlDiv(id='output-container-range-slider'),
-              dccRangeSlider(
-                id='non-linear-range-slider',
-                min = 1950,
-                max = 2016,
-                step=1,
-                marks = list(
-                  '1950'= "1950",
-                  '1955'= "1955",
-                  '1960'= "1960",
-                  '1965'= "1965",
-                  '1970'= "1970",
-                  '1975'= "1975",
-                  '1980'= "1980",
-                  '1985'="1985",
-                  '1990'= "1990",
-                  '1995'="1995",
-                  '2000'="2000",
-                  '2005'="2005",
-                  '2010' ="2010",
-                  '2015'= "2015"
-                ),
-                value = list(1990,2010))))
+dbcCardBody(list(htmlDiv(list(htmlDiv(id='output-container-range-slider'),
+dccRangeSlider(id='non-linear-range-slider',min = 1950,max = 2016,step=1,
+marks = list(
+"1950"= "1950",
+"1955"= "1955",
+"1960"= "1960",
+"1965"= "1965",
+"1970"= "1970",
+"1975"= "1975",
+"1980"= "1980",
+"1985"="1985",
+"1990"= "1990",
+"1995"="1995",
+"2000"="2000",
+"2005"="2005",
+"2010" ="2010",
+"2015"= "2015"),value = list(1990,2010))))
               # plot goes here 
-              
-            
-          )
-        
-      ))
+)))
     
   
 
 # card 5 containing the top slider and second faceted bar chart
-fifth_card = dbcCard(
-  dbcCardBody(list(
-    
-    
-    
-    
-    
-    htmlDiv(
-      list(
-        
-        htmlDiv(id='output-container-range-slider2'),
-        dccRangeSlider(
-          id='non-linear-range-slider2',
-          min = 1950,
-          max = 2016,
-          step=1,
-          marks = list(
-            '1950'= "1950",
-            '1955'= "1955",
-            '1960'= "1960",
-            '1965'= "1965",
-            '1970'= "1970",
-            '1975'= "1975",
-            '1980'= "1980",
-            '1985'="1985",
-            '1990'= "1990",
-            '1995'="1995",
-            '2000'="2000",
-            '2005'="2005",
-            '2010' ="2010",
-            '2015'= "2015"
-          ),
-          value = list(1990,2010))))
+fifth_card = dbcCard(dbcCardBody(list(htmlDiv(
+list(htmlDiv(id="output-container-range-slider2"),
+dccRangeSlider(
+id="non-linear-range-slider2",min = 1950,max = 2016,step=1,
+marks = list(
+"1950"= "1950",
+"1955"= "1955",
+"1960"= "1960",
+"1965"= "1965",
+"1970"= "1970",
+"1975"= "1975",
+"1980"= "1980",
+"1985"="1985",
+"1990"= "1990",
+"1995"="1995",
+"2000"="2000",
+"2005"="2005",
+"2010" ="2010",
+"2015"= "2015"),value = list(1990,2010))))
     # plot goes here 
-    
-    
-  )
-  
-  ))
+)))
 
 # card 6 containing the control card for tab 2
 sixth_card = dbcCard(dbcCardBody(list(generate_control_card_tab2())))
 
-seventh_card = dbcCard(
-  dbcCardBody(list(
-                   
-                   
-                   
-                   
-                   
-                   
-    
-      htmlDiv("Plot for top 10 games goes here 
-      
-      
-      
-      
-      "
+seventh_card = dbcCard(dbcCardBody(list(htmlDiv("Plot for top 10 games goes here"
         # top n games chart goes here
-  )
-)))
+))))
 
 # card 8 containing the data table for the top n games for tab 2
-eight_card = dbcCard(
-  dbcCardBody(
-    list(
-      htmlH5("Top 10 Games Facts Table:")
+eight_card = dbcCard(dbcCardBody(list(htmlH5("Top 10 Games Facts Table:")
       # data table for top 10 games goes here, comes from wrangling
-    )
-  )
-)
+)))
 
 # card 9 for data set description tab 1
 ninth_card = dbcCard(
-  dbcCardBody(
-    
-      htmlDiv(
-        
-          dbcButton(
-            "Click here to view dataset description",
-            id="collapse-button",
-            className="mb-3",
-            color="primary",
-          ),
-          dbcCollapse(
-            dbcCard(dbcCardBody(data_set_description())), id="collapse"
-          )
-        
-      )
-    
-  )
-)
+dbcCardBody(htmlDiv(dbcButton("Click here to view dataset description",id="collapse-button",className="mb-3",color="primary",),
+dbcCollapse(dbcCard(dbcCardBody(data_set_description())), id="collapse"))))
 
 # tab styling features for layout
 tabs_styles = list("height"= "44px")
 tab_style = list(
-  "borderBottom"="1px solid #d6d6d6",
-  "padding"= "6px",
-  "fontWeight"= "bold",
-  "display"="All"
-)
+"borderBottom"="1px solid #d6d6d6",
+"padding"= "6px",
+"fontWeight"= "bold",
+"display"="All")
 
 tab_selected_style = list(
-  "borderTop"= "1px solid #d6d6d6",
-  "borderBottom"= "1px solid #d6d6d6",
-  "backgroundColor"= "#119DFF",
-  "color"= "white",
-  "padding"= "6px"
-)
+"borderTop"= "1px solid #d6d6d6",
+"borderBottom"= "1px solid #d6d6d6",
+"backgroundColor"= "#119DFF",
+"color"= "white",
+"padding"= "6px")
 
 #  set up app stylesheet and server
 app <- Dash$new(external_stylesheets = dbcThemes$BOOTSTRAP)
 
 # layout made up of cards
-app$layout(htmlDiv(list(
-  
-    dbcContainer(
-      
-        
-                htmlDiv(
-                  id="title_top",
-                  className="title on top",
-                  list(title())
-               
-         
-    )), htmlDiv(dccTabs(id="tabs_in_app", value='tab-1',list(
-  
-  dccTab(label='Game Dynamics Over Time', value='tab-1',children=list(dbcRow(list(dbcCol(first_card, width=3), dbcCol(second_card, width=9))), dbcRow(list(dbcCol(third_card, width=3),dbcCol(list(fourth_card,fifth_card), width=9))), dbcRow(dbcCol(ninth_card, width=3))), style=tab_style,
-         selected_style=tab_selected_style),
-  dccTab(label='Top Games', value='tab-2',children=list(dbcRow(list(dbcCol(sixth_card, width=3), dbcCol(list(seventh_card,eight_card), width=9)))),style=tab_style,
-         selected_style=tab_selected_style)))))))
+app$layout(htmlDiv(list(dbcContainer(htmlDiv(id="title_top",className="title on top", list(title()))), htmlDiv(dccTabs(id="tabs_in_app", value="tab-1"
+,list(dccTab(label="Game Dynamics Over Time", value='tab-1',children=list(dbcRow(list(dbcCol(first_card, width=3), dbcCol(second_card, width=9))), dbcRow(list(dbcCol(third_card, width=3),dbcCol(list(fourth_card,fifth_card), width=9))), dbcRow(dbcCol(ninth_card, width=3))), style=tab_style,
+selected_style=tab_selected_style),dccTab(label="Top Games", value="tab-2",children=list(dbcRow(list(dbcCol(sixth_card, width=3), dbcCol(list(seventh_card,eight_card), width=9)))),style=tab_style,
+selected_style=tab_selected_style)))))))
   
   
 # app callbacks
@@ -372,33 +224,28 @@ app$layout(htmlDiv(list(
 
 # 1st year range slider output tab 1
 
-app$callback(
-  list(output('output-container-range-slider', 'children')),
-  list(input('non-linear-range-slider', 'value')),
-  function(value) {
-    
-    value1 <- value[1]
-    value2 <- value[2]
-    string=paste("Years Selected ", value1,"to ", value2)
-    return(list(string))
-  })
+app$callback(list(output("output-container-range-slider", "children")),
+list(input("non-linear-range-slider", "value")),
+function(value) {
+value1 <- value[1]
+value2 <- value[2]
+string=paste("Years Selected ", value1,"to ", value2)
+return(list(string))})
 
 # 2nd year range slider output tab 1
 
 app$callback(
-  list(output('output-container-range-slider2', 'children')),
-  list(input('non-linear-range-slider2', 'value')),
-  function(value) {
-    
-    value1 <- value[1]
-    value2 <- value[2]
-    string=paste("Years Selected ", value1,"to ", value2)
-    return(list(string))
-  })
+list(output("output-container-range-slider2", "children")),
+list(input("non-linear-range-slider2", "value")),
+function(value) {
+value1 <- value[1]
+value2 <- value[2]
+string=paste("Years Selected ", value1,"to ", value2)
+return(list(string))})
 
 
 # collapsible data set description
-# ** Tried to get this to work... can't seem to figure out the r equivalent
+# ** Tried to get this to work... can't seem to figure out the R equivalent
 
 
 #app$callback(
