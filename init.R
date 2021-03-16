b305a7513816dc335e6f98572cb1f5372927f089
Nathan Smith <nwraysmith@gmail.com>
2021-03-16 09:50:29 -0700

    lint init.R update linter workfile move images

--- init.R
+++ init.R
@@ -1,14 +1,16 @@
-# R script to run author supplied code, typically used to install additional R packages
+# R script to run author supplied code, 
+# typically used to install additional R packages
 # contains placeholders which are inserted by the compile script
 # NOTE: this script is executed in the chroot context; check paths!
 
-r <- getOption('repos')
-r['CRAN'] <- 'http://cloud.r-project.org'
-options(repos=r)
+r <- getOption("repos")
+r["CRAN"] <- "http://cloud.r-project.org"
+options(repos = r)
 
 # ======================================================================
 
 # packages go here
-install.packages(c('dash', 'purrr', 'dplyr', 'tidyr', 'here', 'readr', 'ggpubr', 'ggplot2', 'remotes'))
+install.packages(c("dash", "purrr", "dplyr", "tidyr",
+ "here", "readr", "ggpubr", "ggplot2", "remotes"))
 
-remotes::install_github('facultyai/dash-bootstrap-components@r-release')
+remotes::install_github("facultyai/dash-bootstrap-components@r-release")
