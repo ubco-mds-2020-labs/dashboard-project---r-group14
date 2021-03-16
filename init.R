c470e2bd99932de5bc6e28d25076492ca43007d4
Nathan Smith <nwraysmith@gmail.com>
2021-03-16 09:54:54 -0700

    remove Docker linter from workflow

--- init.R
+++ init.R
@@ -1,4 +1,4 @@
-# R script to run author supplied code, 
+# R script to run author supplied code,
 # typically used to install additional R packages
 # contains placeholders which are inserted by the compile script
 # NOTE: this script is executed in the chroot context; check paths!
